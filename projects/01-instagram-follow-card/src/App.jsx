4e662439439aefb97ec73de9b489d6b3bbd4fcfd
salomon.cjl <contrerassalomon6@gmail.com>
2023-08-02 13:26:36 -0300

    instagram card maquetado

---
+++ projects/01-instagram-follow-card/src/App.jsx
@@ -0,0 +1,19 @@
+export function App() {
+    return (
+        <article>
+            <header>
+                <img src="https://unavatar.io/goku" alt="Avatar Goku"/>
+                <div>
+                    <strong>Son Goku</strong>
+                    <span>@Goku</span>
+                </div>
+            </header>
+
+            <aside>
+                <button>
+                    Seguir
+                </button>
+            </aside>
+        </article>
+    )
+}
